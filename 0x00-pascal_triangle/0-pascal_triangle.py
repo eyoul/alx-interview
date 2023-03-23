caed206bfb034aba7ea5500c2bf01eb45059d242
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-03-23 01:42:35 -0700

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
+"""generates a pascal triangle"""
+
 def pascal_triangle(n):
-    """generates a pascal triangle
 
-    Args:
-        n (int): number of rows
-
+    """
     Returns:
         list(list(int)): list of lists of integers
     """
-    triangle = list()
-    if n > 0:
-        for i in range(n):
-            row = [1]
-            for j in range(1, i + 1):
-                row.append(triangle[i - 1][j - 1] +
-                           triangle[i - 1][j] if i != j else 1)
-            triangle.append(row)
-    return triangle
+    if n <= 0:
+        return []
+    
+    pascal_triangle = [0] * n
+
+    for i in range(n):
+        new = [0] * (i + 1)
+        new[0] = 1
+        new[len(new) - 1] = 1
+
+        for j in range (1, i):
+            if j > 0 and j < len(new):
+                x = pascal_triangle[i - 1][j]
+                y = pascal_triangle[i - 1][j - 1]
+                new[j] = x + y
+
+        pascal_triangle[i] = new
+
+    return pascal_triangle
