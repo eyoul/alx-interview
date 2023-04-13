12a427de9bad68913a5e626c6adb945142c5555f
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-04-13 11:16:11 -0700

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -26,13 +26,13 @@ if __name__ == "__main__":
             try:
                 statusC = int(statusC_and_file_s.split()[0])
                 f_size = int(statusC_and_file_s.split()[1])
-                # print (status code: {})
+                # print("Status Code {} size {}".format(statusC, f_size))
                 if statusC in statusC_counter:
                     statusC_counter[statusC] += 1
                 file_size = file_size + f_size
             except:
                 pass
-            printCodes(statusC_counter, file_size)
+        printCodes(statusC_counter, file_size)
     except KeyboardInterrupt:
         printCodes(statusC_counter, file_size)
         raise
