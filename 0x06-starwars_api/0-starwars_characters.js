7deb1dcc2b4a894ec99a2ed1c98e2eb2b90bac91
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-03 04:20:11 -0700

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,7 +3,8 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-request(`https://swapi-api.alx-tools.com/api/films/${movieId}/`, function (error, response, body) {
+request(`https://swapi-api.alx-tools.com/api/films/${movieId}/`,
+function (error, response, body) {
   if (!error && response.statusCode === 200) {
     const characters = JSON.parse(body).characters;
     characters.forEach((characterUrl) => {
