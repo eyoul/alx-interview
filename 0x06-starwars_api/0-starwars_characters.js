91ad8c92a2072a3352963c89205a7431a74d7a6c
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-03 01:55:45 -0700

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,18 +3,16 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-request(`https://swapi.dev/api/films/${movieId}/`, function (error,
-response, body) {
-    if (!error && response.statusCode === 200) {
-        const characters = JSON.parse(bady).characters;
-        characters.forEach((characterUrl) => {
-          request(characterUrl, function (error, response, body) {
-            if (!error && request.statusCode === 200) {
-                const characterName = JSON.parse(body).name;
-                console.log(characterName);
-            }
-          });
-        });
-    }
+request(`https://swapi.dev/api/films/${movieId}/`, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((characterUrl) => {
+      request(characterUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          const characterName = JSON.parse(body).name;
+          console.log(characterName);
+        }
+      });
+    });
+  }
 });
-
