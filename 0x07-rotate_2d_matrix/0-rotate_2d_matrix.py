24050fb0ba43e3a55d6e71f7ffd86b23120b0222
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-10 17:14:26 -0700

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -13,14 +13,14 @@ def rotate_2d_matrix(matrix):
         None
     """
     n = len(matrix)
-    """Transpose the matrix"""
     for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    # Reverse each row
+        for j in range(i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
     for i in range(n):
-        matrix[i] = matrix[i][::-1]
-
-
-rotate_2d_matrix(matrix)
-print(matrix)
+        for j in range(int(n / 2)):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][n-1-j]
+            matrix[i][n-1-j] = temp
