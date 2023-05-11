8c1e3a5c96be8c2c1a5927f654180b9707a99a19
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-10 17:11:45 -0700

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,8 +5,15 @@ Test 0x07 - Rotate 2D Matrix
 
 
 def rotate_2d_matrix(matrix):
+    """
+    Ritate a 2d square matrix 90 degrees
+    Args:
+        matrix (list): 2d square
+    Return: 
+        None
+    """
     n = len(matrix)
-    # Transpose the matrix
+    """Transpose the matrix"""
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
