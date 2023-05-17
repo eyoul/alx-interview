93a96f7440fbe5ea5d50e988fe036a2fdc91b98b
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-17 13:25:26 -0700

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,7 +6,12 @@ Change comes from within
 
 def makeChange(coins, total):
     """
-    Make change coins and total
+    Return the minimum number of coins needed to meet a given total
+    Args:
+        coins (list of ints): a list of coins of different values
+        total (int): total value to be met
+    Return:
+        Number of coins or -1 if meeting the total is not possible
     """
     if total <= 0:
         return 0
@@ -19,20 +24,20 @@ def makeChange(coins, total):
         pass
 
     coins.sort(reverse=True)
-    min_coins = 0
+    coin_count = 0
     for i in coins:
         if total % i == 0:
-            min_coins += int(total / i)
-            return min_coins
+            coin_count += int(total / i)
+            return coin_count
         if total - i >= 0:
             if int(total / i) > 1:
-                min_coins += int(total / i)
-                total = total % i  
+                coin_count += int(total / i)
+                total = total % i
             else:
-                min_coins +=1
+                coin_count += 1
                 total -= i
                 if total == 0:
                     break
     if total > 0:
         return -1
-    return min_coins
+    return coin_count
